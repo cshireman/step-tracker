50a1b5cbd24046ffe795b8b14db32e816bd1c0c0
cshireman <chris@shireman.net>
2025-04-20 19:06:34 -0700

    Initial project addition

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Chris Shireman on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
