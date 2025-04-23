3a300133da478eb21d83e2c52735a55b0cfc9753
cshireman <chris@shireman.net>
2025-04-23 15:02:34 -0700

    hasSeenPermissionPriming and app storage

--- Step Tracker/HealthKitPermissionPrimingView.swift
+++ Step Tracker/HealthKitPermissionPrimingView.swift
@@ -13,6 +13,7 @@ struct HealthKitPermissionPrimingView: View {
     @Environment(\.dismiss) private var dismiss
     
     @State private var isShowingHealthKitPermissions: Bool = false
+    @Binding var hasSeen: Bool
     
     var description = """
     This app displays your step and weight data in interactive charts.
@@ -44,6 +45,10 @@ struct HealthKitPermissionPrimingView: View {
             .tint(.pink)
         }
         .padding(30)
+        .interactiveDismissDisabled()
+        .onAppear {
+            hasSeen = true
+        }
         .healthDataAccessRequest(store: hkManager.store,
                                  shareTypes: hkManager.types,
                                  readTypes: hkManager.types,
@@ -62,6 +67,6 @@ struct HealthKitPermissionPrimingView: View {
 }
 
 #Preview {
-    HealthKitPermissionPrimingView()
+    HealthKitPermissionPrimingView(hasSeen: .constant(true))
         .environment(HealthKitManager())
 }
