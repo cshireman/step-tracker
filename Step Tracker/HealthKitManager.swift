41eae453d2bfa79bf455bb36a536108b4e6b71c1
cshireman <chris@shireman.net>
2025-04-24 10:21:30 -0700

    Initial step chart work

--- Step Tracker/HealthKitManager.swift
+++ Step Tracker/HealthKitManager.swift
@@ -17,6 +17,9 @@ import Observation
         HKQuantityType(.bodyMass)
     ]
     
+    var stepData: [HealthMetric] = []
+    var weightData: [HealthMetric] = []
+    
     func fetchStepCount() async {
         let calendar = Calendar.current
         let today = calendar.startOfDay(for: .now)
@@ -32,6 +35,9 @@ import Observation
                                                                intervalComponents: .init(day: 1))
         
         let stepCounts = try! await stepsQuery.result(for: store)
+        stepData = stepCounts.statistics().map {
+            .init(date: $0.startDate, value: $0.sumQuantity()?.doubleValue(for: .count()) ?? 0)
+        }
     }
     
     func fetchWeights() async {
@@ -49,6 +55,9 @@ import Observation
                                                                intervalComponents: .init(day: 1))
         
         let weights = try! await weightsQuery.result(for: store)
+        weightData = weights.statistics().map {
+            .init(date: $0.startDate, value: $0.mostRecentQuantity()?.doubleValue(for: .pound()) ?? 0)
+        }
     }
     
 //    func addSimulatorData() async {
