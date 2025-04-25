b2701d509722b8db95112a889484d26f0a23b1d7
cshireman <chris@shireman.net>
2025-04-25 13:45:15 -0700

    Adding chart math

---
+++ Step Tracker/Charts/ChartMath.swift
@@ -0,0 +1,26 @@
+//
+//  ChartMath.swift
+//  Step Tracker
+//
+//  Created by Chris Shireman on 4/25/25.
+//
+
+import Foundation
+import Algorithms
+
+struct ChartMath {
+    @discardableResult
+    static func averageWeekdayCount(for metric: [HealthMetric]) -> [WeekdayChartData] {
+        let sortedByWeekday = metric.sorted { $0.date.weekdayInt < $1.date.weekdayInt }
+        let weekdayArray = sortedByWeekday.chunked { $0.date.weekdayInt == $1.date.weekdayInt}
+        
+        var weekdayChartData : [WeekdayChartData] = []
+        for array in weekdayArray {
+            let total = array.reduce(0.0) { $0 + $1.value }
+            let average = total / Double(array.count)
+            weekdayChartData.append(.init(date: array[0].date, value: average))
+        }
+
+        return weekdayChartData
+    }
+}
---
+++ Step Tracker/Extensions/Date+Ext.swift
@@ -0,0 +1,14 @@
+//
+//  Date+Ext.swift
+//  Step Tracker
+//
+//  Created by Chris Shireman on 4/25/25.
+//
+
+import Foundation
+
+extension Date {
+    var weekdayInt: Int {
+        return Calendar.current.component(.weekday, from: self)
+    }
+}
